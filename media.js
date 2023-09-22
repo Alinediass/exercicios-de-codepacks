733ec29e686c92c4244e1ace6794e5ee21f084fe
Aline Dias <alinefds@outlook.com.br>
2023-09-22 17:03:00 -0300

    Adicionamos um novo arquivo de média.js

---
+++ media.js
@@ -0,0 +1,7 @@
+var nota1 = Number(prompt("Digite a primeira nota (entre 0 e 10):"));
+var nota2 = Number(prompt("Digite a segunda nota (entre 0 e 10):"));
+var nota3 = Number(prompt("Digite a terceira nota (entre 0 e 10):"));
+
+var media = (nota1 + nota2 + nota3) / 3;
+
+console.log("A média das notas é: " + media);
